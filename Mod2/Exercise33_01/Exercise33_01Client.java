0200b1915e8b7dba1dab8bed20bd61b394ea7b26
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-12-11 10:34:19 -0700

    small update

--- Mod2/Exercise33_01/Exercise33_01Client.java
+++ Mod2/Exercise33_01/Exercise33_01Client.java
@@ -2,8 +2,8 @@
 // result back from the server
 package com.journaldev.socket;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.lang.ClassNotFoundException;
 import java.net.ServerSocket;
 import java.net.Socket;
